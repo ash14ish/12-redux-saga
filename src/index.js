2b1acc3faf12a76facc15f5d8e1eddd948acc59b
ASHISH <96319346+ash14ish@users.noreply.github.com>
2023-05-02 13:38:47 +0530

    Credentials true comment

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://rem-rest-api.herokuapp.com/api";
 
 const sagaMiddleWare = createSagaMiddleware();
